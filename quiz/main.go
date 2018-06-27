a3126fe6b0ef0ccf2bd039146b7af69cca63ec8c
Matt Boyle <matthew.boyle@crowdcube.com>
2018-06-27 13:35:40 +0100

    gophercise 1, quiz game

---
+++ quiz/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.csv", "csv file with question,answer")
+	timeLimit := flag.Int("limit", 30, "the time limit for the quiz, in seconds.")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		fmt.Printf("Failed to open the CSV file %s \n", *csvFileName)
+		os.Exit(1)
+	}
+
+	r := csv.NewReader(file)
+	lines, error := r.ReadAll()
+
+	if error != nil {
+		fmt.Printf("Couldn't parse the file: %s \n", *csvFileName)
+		os.Exit(1)
+	}
+
+	problems := parseLines(lines)
+
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("problem #%d: %s =\n", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <- answerCh:
+			if answer == p.a {
+				correct++
+			}
+		}
+	}
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: strings.TrimSpace(line[1]),
+		}
+	}
+
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
