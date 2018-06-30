68a94e70b459b0be7bf41118aa2d9a92e966b4d7
Matt Boyle <matthew.boyle@crowdcube.com>
2018-06-30 17:17:30 +0100

    make your own adventure v1

---
+++ makeyourownadventure/cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"gophercises/makeyourownadventure"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := flag.Int("port", 3000, "port")
+	file := flag.String("file", "story.json", "The json file for the choose your own adventure story.")
+	flag.Parse()
+	fmt.Printf("Using the story in %s \n", *file)
+
+	f, err := os.Open(*file)
+
+	story, err := makeyourownadventure.JsonStory(f)
+
+	if err != nil {
+		panic(err)
+	}
+
+	h := makeyourownadventure.NewHandler(story)
+
+	fmt.Printf("Starting the server on %d", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
+
+}
